ac80ca3b88686fc2a267c2807b14e04a16a3d6cb
Alex <ahivent@gmail.com>
2017-06-14 23:48:23 -0500

    Added basic picamera & filename support

--- developement/spibox0.py
+++ developement/spibox0.py
@@ -6,10 +6,15 @@
 import RPi.GPIO as GPIO
 import time
 import picamera
+import datetime
 
 
 # set GPIO pin numbering, BOARD or BCM and input pin/channels
 try:
+    def get_file_name():
+        """Use datetime to create filename"""
+        return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+
     GPIO.setmode(GPIO.BCM)
     PIR_Pin = 4
     GPIO.setup(PIR_Pin, GPIO.IN)
@@ -28,6 +33,7 @@ try:
         if GPIO.input(PIR_Pin):
             print(GPIO.input(4))
             print("Motion Detected!")   # input is high
+            camera.capture(get_file_name(), format='jpeg')
         time.sleep(1)
 
 except KeyboardInterrupt:
